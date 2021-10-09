c1771f6d99072d75a6ca7e9a76a3618165f6bed7
Y0DS <y0ds@nykredit.dk>
2021-10-09 13:55:21 +0200

    Added Weather Server
    (cherry picked from commit 9fcaf17dbd60f3df151ce761a1f3447414992006)

---
+++ src/test/java/dk/bec/autopro/workshop/connector/WeatherServerConnectorTest.java
@@ -0,0 +1,12 @@
+package dk.bec.autopro.workshop.connector;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherServerConnectorTest {
+
+    @Test
+    void shouldFetchTemperatureWithinTimeLimit() {
+    }
+}
